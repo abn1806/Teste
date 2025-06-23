2f43b5b234dd8d4c9050724529a93edc21ddcee6
Abner Silva <abnersilvadearaujo@gmail.com>
2025-06-23 13:41:08 -0300

    Removendo o arquivo teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-print("Hello! Whats your name?")
-name = "Abner"
-print(f"Nice to meet you, {name}!")
-print("How old are you? ")
-age = 31
-bot_age = 3
-age_difference = age - bot_age
-
-print(f"Vocé é {age_difference} anos mais velho que eu. Eu tenho apenas {bot_age} anos! ")
